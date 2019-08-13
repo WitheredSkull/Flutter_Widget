87ae386cb9671d222f115e4df43c311c9d0cbcca
daniel <599837944@qq.com>
2019-08-13 20:44:43 +0800

    提交DefaultText、

--- lib/view/widget/text/TextWidget.dart
+++ lib/view/widget/text/TextWidget.dart
@@ -7,23 +7,36 @@ class TextWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
-      ///设置文字对齐
+
+        ///设置文字对齐
         textAlign: TextAlign.center,
+
         ///是否自动处理多出来的文字(条件允许下多出来的文字会自动换行)
         softWrap: false,
+
         ///设置文字超出布局之后的处理方式
         overflow: TextOverflow.ellipsis,
+
         ///设置最大行数
         maxLines: 1,
+
         ///TextWidthBasis.longestLine设置文本宽度为最长，TextWidthBasis.parent文本宽度基于父部件宽度
         textWidthBasis: TextWidthBasis.parent,
+
         ///设置文本样式（后面的child树都会根据这个style变化）
         style: TextStyle(
             color: Colors.lightBlue,
             backgroundColor: Colors.white,
             fontSize: 20,
-            decoration: TextDecoration.lineThrough),
+            decoration: TextDecoration.underline),
+
         ///设置子部件
-        child: Text("DefaultTextStyle"));
+        child: Column(
+          children: <Widget>[
+            Text("DefaultTextStyle"),
+            Text("这是三个使用了DefaultTextStyle的文字部件"),
+            Text("可以明显看出样式是一毛一样的")
+          ],
+        ));
   }
 }
